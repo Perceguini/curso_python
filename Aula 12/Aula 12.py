dfb3318a75c9692290c80f67bdaa8f97bf1888d4
Leh Perceguini <perceguini26@gmail.com>
2022-05-04 19:00:35 -0300

    Aula 12. Operaderes Logicos and, or, not in e not in.

--- Aula 12/Aula 12.py
+++ Aula 12/Aula 12.py
@@ -2,4 +2,22 @@
 Operadores Lógicos - Aula 4
 and, or, not
 in e not in
+
+(Verdadeiro e Falso) = Falso
+comparacao1 and comparacao2
+
+Verdadeiro ou verdadeiro
+comp1
+OR
+comp2
 """
+
+
+a = 2
+b = 3
+
+if not b > a:
+    print('B é maior do que A')
+
+else:
+    print('A é maior do que B')

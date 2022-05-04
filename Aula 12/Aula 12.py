221bb8b2f6dc4d7f883985158d9b932cc869cae8
Leh Perceguini <perceguini26@gmail.com>
2022-05-04 19:59:41 -0300

    Aula 12. Operaderes Logicos and, or, not in e not in. Criado login de usuario e senha.

--- Aula 12/Aula 12.py
+++ Aula 12/Aula 12.py
@@ -10,8 +10,6 @@ Verdadeiro ou verdadeiro
 comp1
 OR
 comp2
-"""
-
 
 a = 2
 b = 3
@@ -21,3 +19,35 @@ if not b > a:
 
 else:
     print('A é maior do que B')
+
+a = '32'
+b = 1
+
+if not b:
+    print('Por favor preencha o valor de B')
+
+nome = 'Leticia'
+
+if 'o' in nome:
+    print('existe')
+else:
+    print('Não existe')
+
+nome = 'Perceguini'
+
+if 'Per' not in nome:
+    print('execultei')
+else:
+    print('Existe o texto')
+"""
+usuario = input('Nome de usuário:  ')
+senha = input('senha do usuário:  ')
+
+usuario_bd = 'leticia '
+senha_bd = '123456'
+
+if usuario_bd == usuario and senha_bd == senha:
+    print('você está logado no sistema')
+
+else:
+    print('Usuário ou senha inválida')

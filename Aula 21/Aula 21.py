fe005869acd7d35a7e13cea1c654096d45a35f01
Leh Perceguini <perceguini26@gmail.com>
2022-05-09 17:57:04 -0300

    Aula 21:
    Interando strins com while em python.
    
    Valor interável

---
+++ Aula 21/Aula 21.py
@@ -0,0 +1,24 @@
+"""
+Interando strins com while em python.
+*Índces
+*0123456789...............33
+"""
+# 0123456789...............33
+
+frase = 'O rato roeu a roupa do rei de Roma' # Valor iterável
+tamanho_frase = len(frase)
+contador = 0
+nova_string = ''
+
+input_do_usuario = input('Qual letra deseja colocar maiúscula:  ')
+
+while contador < tamanho_frase:
+    letra = frase[contador]
+    if letra == input_do_usuario :
+        nova_string += input_do_usuario.upper()
+    else:
+        nova_string += letra
+
+    contador += 1
+
+print(nova_string)

cd89ae6c5921c6a964bd2a65cff14a8157981cdd
Leh Perceguini <perceguini26@gmail.com>
2022-05-16 10:21:49 -0300

    Aula 29 -
    Expressão condicional com operador OR

---
+++ Aula 29/Aula 29.py
@@ -0,0 +1,18 @@
+"""
+Expressão condicional com operador OR
+"""
+"""
+nome = input('Qual o seu nome?  ')
+
+print(nome or 'Você não digitou nada!')
+"""
+a = 0
+b = Nome
+c = False
+d = []
+e = {}
+f = 22
+g = 'Luiz'
+
+variavel = a or b or c or d or e or f or g
+print(variavel)

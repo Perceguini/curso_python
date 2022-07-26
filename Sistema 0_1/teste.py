706b93e556c213d6ccd14eb6924e3d7474feadac
Leh Perceguini <perceguini26@gmail.com>
2022-07-26 01:14:45 -0300

    Resolução -
    Resolução de kairo para codigo

---
+++ Sistema 0_1/teste.py
@@ -0,0 +1,49 @@
+"""
+Uma lista de lista de números inteiros
+As listas internas tem tamanho de 10 elementos
+As listas internas comtem  números entre 1 a 10 e eles podem ser duplicados
+
+Exercicio:
+- Crie uma função que encontra o primeiro duplicado, considerando o segundo numero
+como a duplicação. Retorne a duplicação cosiderada.
+Requisitos:
+A ordem do numero duplicados é considerada a partir da segunda ocorrência do numero, ou seja,
+ o número duplicado em si(o número duplicado em si)
+Exemplo:
+[1, 2, 3, -> 3 <-, 2, 1] -> 1,2 e 3 são duplicados (retorne 3)
+[1,2,3,4,5,6] -> retorne -1 (não tem duplicados )
+Se não encontrar duplicados na lista retorne -1
+"""
+
+lista_de_lista_de_inteiros = [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    [9, 1, 8, 9, 9, 7, 2, 1, 6, 8],
+    [1, 3, 2, 2, 8, 6, 5, 9, 6, 7],
+    [3, 8, 2, 8, 6, 7, 7, 3, 1, 9],
+    [4, 8, 8, 8, 5, 1, 10, 3, 1, 7],
+    [1, 3, 7, 2, 2, 1, 5, 1, 9, 9],
+    [10, 2, 2, 1, 3, 5, 10, 5, 10, 1],
+    [1, 6, 1, 5, 1, 1, 1, 4, 7, 3],
+    [1, 3, 7, 1, 10, 5, 9, 2, 5, 7],
+    [4, 7, 6, 5, 2, 9, 2, 1, 2, 1],
+    [5, 3, 1, 8, 5, 7, 1, 8, 8, 7],
+    [10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+]
+
+
+def buscar_duplicados():
+    for lista in lista_de_lista_de_inteiros:
+        print(lista)
+        print_value = []
+            filtered = list(filter(lambda numero: numero == num, lista))
+            if len(filtered) >= 2:
+                if num not in print_value:
+                    print_value.append(num)
+        if len(print_value) > 0:
+            print(f'Repetidos: {print_value}')
+        else:
+            print("-1")
+
+        for num in lista:
+
+buscar_duplicados()

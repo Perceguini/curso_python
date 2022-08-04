76ce50b155fd685c292712dfbe31949e8a2535ff
Leh Perceguini <perceguini26@gmail.com>
2022-08-04 20:19:45 -0300

    Aula 50 - Comportamento de geradores e interadores
    Como pode ser usado os gerenciadores e interadores dentro de  Listas, Tuples e Str

---
+++ Aula 50/Aula 50.py
@@ -0,0 +1,17 @@
+# Comportamento de geradores e interadores
+
+# Listas, Tuples e Str -> Sequencias -> Iteravel.
+nome = 'Leticia'
+iterador = iter(nome)
+print(next(iterador))
+
+#for letra in nome:  # O for converte em tempo de execução a srt em um iterador.
+#    print(letra)
+
+#print('-' * 10)
+
+#nome = 'Leticia'
+#for letra in nome:
+ #   print(letra)
+
+#print(nome)

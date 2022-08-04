a0972ec4875b10a78cf265b3b6e215db762937a2
Leh Perceguini <perceguini26@gmail.com>
2022-08-04 20:21:52 -0300

    Aula 49- Geradores, interadores e iteraveis em python
    melhor forma de criar um gerador de forma rápida e fazer a troca da list Comp.

--- Aula 49/Aula 49.py
+++ Aula 49/Aula 49.py
@@ -9,4 +9,5 @@ l2 = (x for x in range(10000))
 for v in l2:
     print(v)
 #Simplificando: a melhor forma de criar um gerador de forma rápida e fazer
-#a troca da list Comprehension seria trocar os colchetes para parênteses.
+#a troca da list Comp
+# rehension seria trocar os colchetes para parênteses.
